690ba7e3f7a84fd5c5a3a09435a5a94f93826036
MatheusRocha1 <rochinha154@gmail.com>
2022-09-16 09:26:06 -0300

    feat: start gameView mobile

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -5,23 +5,25 @@ import { THEME } from '../../theme';
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count: {
+    ads: number;
+  }
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps { 
   data: GameCardProps;
 }
 
-export function GameCard({data}: Props) {
+export function GameCard({data, ...rest}: Props) {
   return (
-    <TouchableOpacity style={styles.container}>
-      <ImageBackground style={styles.cover} source={data.cover}>
+    <TouchableOpacity style={styles.container} {...rest}>
+      <ImageBackground style={styles.cover} source={{ uri: data.bannerUrl }}>
 
       <LinearGradient style={styles.footer} colors={THEME.COLORS.FOOTER}>
-        <Text style={styles.name}>{data.name}</Text>
-        <Text style={styles.ads}>{data.ads} anuncios</Text>
+        <Text style={styles.name}>{data.title}</Text>
+        <Text style={styles.ads}>{data._count.ads} anuncios</Text>
       </LinearGradient>
 
       </ImageBackground>
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { View, Image, FlatList } from 'react-native';
+
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Heading } from '../../components/Heading'
-import { GameCard } from '../../components/GameCard';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
-
-import { GAMES } from '../../utils/games';
 import { styles } from './styles';
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([])
+
+  useEffect(() => {
+    fetch('http://192.168.1.11:3333/games')
+    .then((response) => response.json())
+    .then(data => setGames(data))
+  },[])
+
   return (
     <View style={styles.container}>
       <Image source={logoImg} style={styles.logo} />
       <Heading title="Encontre seu duo!" subtitle="Selecione o game que deseja jogar..."/>
       
       <FlatList 
-        data={GAMES} 
+        data={games} 
         keyExtractor={ item => item.id } 
         renderItem={({ item })=> (
           <GameCard 
