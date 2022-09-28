5810926f875261264305ff6509d79932baca0f37
MatheusRocha1 <rochinha154@gmail.com>
2022-09-27 21:44:33 -0300

    feat: add routes configs and create game page on mobile

--- mobile/src/components/Background/index.tsx
+++ mobile/src/components/Background/index.tsx
@@ -1,4 +1,4 @@
-import { ImageBackground } from "react-native";
+import { ImageBackground, Text } from "react-native";
 import { styles } from './styles'
 import backgroundImg from '../../assets/background-galaxy.png'
 
